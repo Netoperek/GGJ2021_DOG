77871e8ffe1615952254d03276da548e7de98892
Netoperek <q.krzysztof-chylinski.p@wp.pl>
2021-01-30 01:34:21 +0100

    [Netoperek] Added Player dialogue

---
+++ Dog Paradais/Assets/Netoperek/LogicMechanic/ClickAdnMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAdnMove : MonoBehaviour
+{
+    Rigidbody2D rg;
+    [SerializeField] GameObject swich;
+    [SerializeField] GameObject moveObject;
+    Transform startPoint;
+    [SerializeField] Transform targetPoint;
+    [Tooltip("Tag obejektu który ma to uruchomić")] [SerializeField] string interactiveObject;
+    [SerializeField] bool isLoop = false;
+    private void Awake()
+    {
+        startPoint = moveObject.transform;
+        if (TryGetComponent(out Rigidbody2D rg))
+        {
+            this.rg = rg;
+        }
+        else
+        {
+            Debug.LogError(gameObject.name + " Brak komponentu Rigidbody 2D dla: " + moveObject.name);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == interactiveObject)
+        {
+            StartCoroutine(Move());
+        }
+    }
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            if (isLoop)
+            {
+                rg.MovePosition( Vector3.MoveTowards(moveObject.transform.position, targetPoint.position, 0.01f));
+            }
+            else
+            {
+
+            }
+            //yield return new WaitForFixedUpdate();
+            yield return null;
+        }
+    }
+}
---
+++ Dog Paradais/Assets/Netoperek/PlayerDialogue/PlayerDialogue.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDialogue : MonoBehaviour
+{
+    [Header("Ikony")]
+    [SerializeField] List<Sprite> icons;
+    [SerializeField] float speedDialogue;
+    [HideInInspector] [SerializeField] Image icon;
+    [Header("Chumrka")]
+    [HideInInspector] [SerializeField] Image dialogueImage;
+    [HideInInspector] [SerializeField] GameObject canvas;
+    [SerializeField] float speedFade;
+    Color color;
+    [Tooltip("Szybkość pojawiania się")] [SerializeField] float fade = 0f;
+    int index = 0;
+    bool isVisible = false;
+
+    private void Awake()
+    {
+        color = new Color(0f, 0f, 0f, fade);
+        icon.enabled = false;
+        canvas.SetActive(true);
+        StartCoroutine(OpenDialogue());
+    }
+    IEnumerator Dialogues()
+    {
+        while (true)
+        {
+            
+                if (index < icons.Count)
+                {
+                    Debug.Log(icons.Count);
+                    icon.sprite = icons[index];
+                    index++;
+                }
+                else
+                {
+                    canvas.SetActive(false);
+                    yield break;
+                }
+            
+            yield return new WaitForSeconds(speedDialogue);
+            
+        }
+    }
+    IEnumerator OpenDialogue()
+    {
+        while (true)
+        {
+            if (1f >= dialogueImage.color.a)
+            {
+                dialogueImage.color += color;
+            }
+            else
+            {
+                icon.enabled = true;
+                StartCoroutine(Dialogues());
+                yield break;
+            }
+
+            yield return new WaitForSeconds(speedFade);
+
+        }
+    }
+
+
+}
